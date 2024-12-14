8baeea1d622b12e4dd0f60aa0f3f9f51e9fb18a3
himanshu <himanshub166@gmail.com>
2024-12-14 16:15:48 +0530

    error handling for webdriver

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 # selenium
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import  NoSuchElementException, TimeoutException, WebDriverException
 import time
 
 
@@ -38,24 +39,47 @@ class ECommerceWebsiteScrapping:
 
     # Prepare a soup from html content
     def html_to_soup(self,content):
-        soup = BeautifulSoup(content,'html5lib')
-        return soup
+        try:
+            soup = BeautifulSoup(content,'html5lib')
+            return soup
+        except Exception as e:
+            print('Unexpected error occour : ', str(e))
+            return ''
 
     # update self.categories through selenium
     def fetch_categories(self):
-        driver = webdriver.Chrome()
-        driver.get(self.url)
-        time.sleep(3)
-        dropdown_element = driver.find_element('id', 'gh-cat')
-        select = Select(dropdown_element)
+        try:
+            driver = webdriver.Chrome()
+            driver.get(self.url)
+            time.sleep(3)
+            dropdown_element = driver.find_element('id', 'gh-cat')
+            select = Select(dropdown_element)
 
-        # Get all options
-        options = select.options
+            # Get all options
+            options = select.options
 
-        list_of_categories = [option.text.strip() for option in options]
-        self.categories = list_of_categories
-        return self.categories
-    
+            list_of_categories = [option.text.strip() for option in options]
+            self.categories = list_of_categories
+            return self.categories
+        except NoSuchElementException as e:
+            print('No select element found : ', str(e))
+            return self.categories
+        
+        except TimeoutException as e:
+            print('Pages takes too long time to load : ', str(e))
+            return self.categories
+        
+        except WebDriverException as e:
+            print('webdriver encounter an issue :', str(e))
+            return self.categories
+        
+        except Exception as e:
+            print('An unexpected issue : ',str(e))
+            return self.categories
+        
+        finally:
+            if driver:
+                driver.quit()
 
 
 def main():
